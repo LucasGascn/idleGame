dbd921f122775b22323179c7bd7e1c3d049a1d57
Lucas <lucas.gascoin@edu.esiee-it.fr>
2022-10-06 14:17:52 +0200

    something

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,12 +1,15 @@
 <script>
     import Home from '../components/Home.vue'
+    import TheWelcome from '../components/TheWelcome.vue';
     export default{
         components:{
-            Home
+            Home,
+            TheWelcome
         }
     }
 </script>
 
 <template>
     <Home/>
+    <TheWelcome/>
 </template>
